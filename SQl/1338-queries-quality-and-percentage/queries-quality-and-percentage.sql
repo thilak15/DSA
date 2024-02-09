ae24125ca6e5832f7548fc42c46cad7f441dfaff
thilak15 <thilakreddy125@gmail.com>
2024-02-09 14:47:13 -0500

    Delete SQl/1338-queries-quality-and-percentage directory

--- SQl/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-select query_name,round(avg((rating/position)),2) as quality,
-round((count(case when rating<3 then query_name end)/count(query_name))*100,2) as poor_query_percentage
-from Queries 
-where query_name is not null
-group by query_name
