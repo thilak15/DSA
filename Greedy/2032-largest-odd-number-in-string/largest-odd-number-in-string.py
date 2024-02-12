6a2cf0adcfcbd0ab0fa2d42af0f577f6a1a404de
thilak15 <thilakreddy125@gmail.com>
2024-02-12 17:12:26 -0500

    Time: 53 ms (55.03%) | Memory: 18 MB (64.35%) - LeetSync

--- Greedy/2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ Greedy/2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        i=0
+        """i=0
         k=0
         c=0
         while i <len(num):
@@ -11,4 +11,8 @@ class Solution:
         if c>0:
             return num[:k+1]
         else:
-            return ""
+            return """""
+        for i in range(len(num)-1,-1,-1):
+            if int(num[i])%2!=0:
+                return num[:i+1]
+        return ""
