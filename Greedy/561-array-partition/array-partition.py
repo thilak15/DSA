e99754f6ece1eb116744f2cf7123afb8c01118dc
thilak15 <thilakreddy125@gmail.com>
2024-02-13 16:12:10 -0500

    Time: 209 ms (92.23%) | Memory: 20 MB (64.50%) - LeetSync

--- Greedy/561-array-partition/array-partition.py
+++ Greedy/561-array-partition/array-partition.py
@@ -1,7 +1,9 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
+        nums.sort() # Time Complexity O(nlogn)
         s=0
         for i in range(0,len(nums)-1,2):
             s+=nums[i]
         return s
+        # Time complexity O(nlogn)
+        # Space complexity O(1)
