d1352b3c5266eb03b22425e4292068a04cb909b2
thilak15 <thilakreddy125@gmail.com>
2024-02-15 13:51:28 -0500

    Time: 124 ms (62.67%) | Memory: 88.1 MB (57.34%) - LeetSync

---
+++ Binarytree/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        root_val=preorder[0]
+        root=TreeNode(root_val)
+        i = inorder.index(root_val)
+
+        root.left=self.buildTree(preorder[1:i+1],inorder[:i])
+        root.right=self.buildTree(preorder[i+1:],inorder[i+1:])
+
+        return root
+    # Time Complexity O(n^2)
